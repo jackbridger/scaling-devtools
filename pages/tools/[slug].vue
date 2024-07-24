f48b10dd3ee2d49e41fd53764e234ff9dc978599
Ostap Brehin <osbre@protonmail.com>
2024-07-24 02:27:26 +0100

    Remove unnecessary `watch` from `[slug].vue`

--- pages/tools/[slug].vue
+++ pages/tools/[slug].vue
@@ -9,7 +9,6 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import NavBar from '~/components/NavBar.vue'
 import ToolInfo from '~/components/ToolInfo.vue'
@@ -18,17 +17,10 @@ import Stats from '~/components/Stats.vue'
 import Header from '~/components/Header.vue'
 import { tools } from '~/data/tools.js'
 
-const tool = ref(null)
-const breadcrumbs = ref([])
 const route = useRoute()
-
-watch(() => route.params.slug, (newSlug) => {
-  if (newSlug) {
-    tool.value = tools.find(t => t.slug === newSlug)
-    breadcrumbs.value = [
-      { name: 'Tools', href: '/tools', current: false },
-      { name: tool.value.name, href: `/tools/${tool.value.slug}`, current: true }
-    ]
-  }
-}, { immediate: true })
+const tool = tools.find(t => t.slug === route.params.slug)
+const breadcrumbs = [
+  { name: 'Tools', href: '/tools', current: false },
+  { name: tool.name, href: `/tools/${tool.slug}`, current: true }
+]
 </script>
