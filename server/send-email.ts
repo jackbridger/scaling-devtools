04e069b3274d32d218913539717c320786095a01
jackbridger <jacksbridger@gmail.com>
2024-08-12 19:50:40 -0700

    working resend

--- server/send-email.ts
+++ server/send-email.ts
@@ -4,12 +4,15 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to: string, subject: string, html: string) => {
     try {
+        console.log(to, subject, html);
+        console.log("sending email");
         const data = await resend.emails.send({
-            from: 'Scaling DevTools <updates@scalingdevtools.com>',
+            from: 'Scaling DevTools <noreply@updates.scalingdevtools.com>',
             to: [to, 'jack@scalingdevtools.com'],
             subject: subject,
             html: html,
           });
+          console.log(data);
           return data
     } catch (error) {
         console.log(error);
