4c699355ba5f4a9b4280b4738a7b02b2f41b066a
jackbridger <jacksbridger@gmail.com>
2024-08-12 23:36:48 -0700

    save -> submit

--- pages/add-cofounder.vue
+++ pages/add-cofounder.vue
@@ -67,7 +67,7 @@
 
       <div class="mt-6 flex items-center justify-end gap-x-6">
         <button type="button" class="text-sm font-semibold leading-6 text-gray-900" @click="cancel">Cancel</button>
-        <button type="submit" class="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Save</button>
+        <button type="submit" class="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Submit</button>
       </div>
     </div>
   </form>
