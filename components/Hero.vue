242cac6916d8923b248f7678fd9aa952d471a80c
jackbridger <jacksbridger@gmail.com>
2024-10-14 14:35:47 +0100

    upper case logo

--- components/Hero.vue
+++ components/Hero.vue
@@ -27,8 +27,8 @@
         <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div class="flex items-center justify-between">
             <a href="#" class="-m-1.5 p-1.5">
-              <span class="sr-only">Scaling DevTools</span>
-              <h1 class="h-8 w-auto font-mono">Scaling DevTools</h1>
+              <span class="sr-only">SCALING DEVTOOLS</span>
+              <h1 class="h-8 w-auto font-mono">SCALING DEVTOOLS</h1>
               <!-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=600" alt="" /> -->
             </a>
             <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -3,8 +3,8 @@
     <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
         <a href="/" class="-m-1.5 p-1.5">
-          <span class="sr-only">Scaling DevTools</span>
-          <h1 class="h-8 w-auto font-logo text-2xl">Scaling DevTools</h1>
+          <span class="sr-only">SCALING DEVTOOLS</span>
+          <h1 class="h-8 w-auto font-logo text-2xl">SCALING DEVTOOLS</h1>
         </a>
       </div>
       <div class="flex lg:hidden">
@@ -22,8 +22,8 @@
       <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
         <div class="flex items-center justify-between">
           <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Scaling DevTools</span>
-            <h1 class="h-8 w-auto font-mono">Scaling DevTools</h1>
+            <span class="sr-only">SCALING DEVTOOLS</span>
+            <h1 class="h-8 w-auto font-mono">SCALING DEVTOOLS</h1>
           </a>
           <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
             <span class="sr-only">Close menu</span>
